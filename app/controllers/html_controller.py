75563715058a2431524ae01a00946f17de7e345e
Sudha Priyadarshini <priyadarshini.sudha3@gmail.com>
2025-09-22 23:43:43 +0530

    initdb()

--- app/controllers/html_controller.py
+++ app/controllers/html_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from app.core.database import init_db
 
 # Router for HTML views
 router = APIRouter()
@@ -10,6 +11,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def serve_index(request: Request):
+    init_db()
     return templates.TemplateResponse("index.html", {"request": request})
 
 @router.get("/property_details/189 Hoxton Street", response_class=HTMLResponse)
