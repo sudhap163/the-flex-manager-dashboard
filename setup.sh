d0df1dbe16f5213602ec047e31148ec5ed2ade23
Sudha Priyadarshini <priyadarshini.sudha3@gmail.com>
2025-09-22 20:52:08 +0530

    epel

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
 dnf install sqlite-devel -y
 dnf install epel-release -y
 dnf install python3.13 -y
